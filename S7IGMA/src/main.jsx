e2f423db86ac10178ff98e562f129ca9691e5063
licahsun <hsunclan@gmail.com>
2025-09-03 16:50:32 +0800

    0903-header切板完成

--- S7IGMA/src/main.jsx
+++ S7IGMA/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import App from './App.jsx'
+// import App from './pages/test'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
