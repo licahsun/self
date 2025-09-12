f56a91f24334cae4de64f9b0d7cf6d5b95700ccf
licahsun <hsunclan@gmail.com>
2025-09-12 13:03:38 +0800

    0912

--- S7IGMA/src/pages/Character.jsx
+++ S7IGMA/src/pages/Character.jsx
@@ -3,19 +3,27 @@ import charaItem from "../../public/images/charaItem01.avif"
 
 const Character = () => {
     return (
-        <div className="Characters">
-        <div className="Character">
+        <div className="charaBg">
             <h2>CHARACTER</h2>
+            <div className="Characters">
+                <div className="Character">
 
-            <div className="Ch1">
-            <div className="ch_swiper"></div>
-                <h3>Sharo</h3>
-                <img className="charaItem01" src={charaItem} alt="" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum placeat quaerat amet. Impedit architecto, laudantium totam et beatae at, sint dicta velit maiores ullam vitae ad nisi debitis? Eveniet fuga fugiat iste fugit aliquam omnis eius dolorem ducimus, vero aut numquam rerum dolor tenetur a exercitationem eos temporibus iure placeat illum. Labore doloremque magnam culpa dicta perspiciatis suscipit temporibus iusto?</p>
+                    <div className="Ch1">
+                        <div className="ch_swiper"></div>
+                        <h3>Sharo</h3>
+                        <img className="charaItem01" src={charaItem} alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum placeat quaerat amet. Impedit architecto, laudantium totam et beatae at, sint dicta velit maiores ullam vitae ad nisi debitis? Eveniet fuga fugiat iste fugit aliquam omnis eius dolorem ducimus, vero aut numquam rerum dolor tenetur a exercitationem eos temporibus iure placeat illum. Labore doloremque magnam culpa dicta perspiciatis suscipit temporibus iusto?</p>
+                    </div>
+
+                    {/* <div className="Ch2">
+                        <div className="ch_swiper"></div>
+                        <h3>Rin</h3>
+                        <img className="charaItem01" src={charaItem} alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum placeat quaerat amet. Impedit architecto, laudantium totam et beatae at, sint dicta velit maiores ullam vitae ad nisi debitis? Eveniet fuga fugiat iste fugit aliquam omnis eius dolorem ducimus, vero aut numquam rerum dolor tenetur a exercitationem eos temporibus iure placeat illum. Labore doloremque magnam culpa dicta perspiciatis suscipit temporibus iusto?</p>
+                    </div> */}
+
+                </div>
             </div>
-            
-            <div className="charaBg"></div>
-        </div>
         </div>
     )
 }
