3d125e45c670b1f223640f55cd4fdc4f30e43e72
licahsun <hsunclan@gmail.com>
2025-09-27 11:14:43 +0800

    0927

--- S7IGMA/src/pages/Footer.jsx
+++ S7IGMA/src/pages/Footer.jsx
@@ -1,6 +1,5 @@
 import "../scss/_Footer.scss"
 import Logo from "../assets/images/s7igma.svg"
-import ScrollToTop from "../components/ScrollToTop"
 import ScrollToTop2 from "../components/ScrollToTop2"
 
 
